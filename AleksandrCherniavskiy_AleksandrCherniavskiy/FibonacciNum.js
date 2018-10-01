2f14b6f3e12dbd816b393ebc7ede8df8d1f2f5e9
AleksandrCherniavskyi <aleksandr.cherniavskyi28@gmail.com>
2018-10-01 16:09:07 +0300

    Rename variable arr as arrayOfElementsFibonacci

--- AleksandrCherniavskiy_AleksandrCherniavskiy/FibonacciNum.js
+++ AleksandrCherniavskiy_AleksandrCherniavskiy/FibonacciNum.js
@@ -2,13 +2,12 @@ function fibonacciNum(numberOfElements) {
     if (numberOfElements == 1) return [1];
     if (numberOfElements == 2) return [1, 1];
 
-    var arr = [1, 1];
+    var arrayOfElementsFibonacci = [1, 1];
     for (var i = 2; i < numberOfElements; i++) {
-        arr[i] = arr[i -1] + arr[i - 2];
+        arrayOfElementsFibonacci[i] = arrayOfElementsFibonacci[i -1] + arrayOfElementsFibonacci[i - 2];
     }
 
-    return arr;
+    return arrayOfElementsFibonacci;
 }
 
 console.log( fibonacciNum(12) );
-   
