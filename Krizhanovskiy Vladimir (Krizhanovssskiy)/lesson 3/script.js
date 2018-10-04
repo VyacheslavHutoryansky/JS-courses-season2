660afa54ad8d5a0ec385b9b48b29ae71358176f7
Krizhanovssskiy <Krizhanovssskiy@gmail.com>
2018-10-04 09:10:44 +0300

    The function returns a new array with objects.

--- Krizhanovskiy Vladimir (Krizhanovssskiy)/lesson 3/script.js
+++ Krizhanovskiy Vladimir (Krizhanovssskiy)/lesson 3/script.js
@@ -1,7 +1,17 @@
 window.onload = function () {
 
     /////  Destruct properties from JSON
-    let {requestId,
+
+    // const {requestId} = task_1,
+    //      {result: [{accountNumber}]} =  task_1,
+    //      {result: [{balance: {otb}}]} = task_1,
+    //      {result: [{cards: [{ucid}]}]} = task_1,
+    //      {result: [{cards: [{expireDate}]}]} = task_1,
+    //      {result: [{cards: [{expireDate: {year}}]}]} = task_1,
+    //      {result: [{overdraftInfo: {lastCloseDayOverBalance}}]} = task_1,
+    //      {result: [{bank: {id}}]} = task_1;
+
+    const {requestId,
         result:
         [{
             accountNumber,
@@ -25,6 +35,25 @@ window.onload = function () {
     console.log('lastCloseDayOverBalance - ' + lastCloseDayOverBalance);
     console.log('Id - ' + id);
 
+
+    //// The function returns a new array with objects.
+
+    const newArray = [];
+
+    function objPrice(id, name, avaragePrice) {
+        this.id = id;
+        this.name = name;
+        this.avaragePrice = avaragePrice;
+
+    }
+
+    for (let i = 0; i < task_2.length; i++) {
+        const {id, name, firstPrice, secondPrice} = task_2[i];
+        const avaragePrice = (firstPrice + secondPrice) / 2;
+        newArray[i] = new objPrice(id, name, avaragePrice);
+    }
+
+    console.log(newArray);
 
 
 };
