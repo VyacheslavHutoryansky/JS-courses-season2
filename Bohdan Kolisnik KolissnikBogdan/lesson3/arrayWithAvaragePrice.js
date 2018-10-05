6ba7eb517d17cb0ad9e602d5a4167d8021a9e6b5
KolissnikBogdan <kamasaikii22@gmail.com>
2018-10-05 12:48:07 +0300

    Kolisnik Bohdan Lesson_3

---
+++ Bohdan Kolisnik KolissnikBogdan/lesson3/arrayWithAvaragePrice.js
@@ -0,0 +1,70 @@
+let users = [
+    {
+        "id": 1,
+        "name": "Sprinter 2500",
+        "firstPrice": 8172,
+        "secondPrice": 9273
+    },
+    {
+        "id": 2,
+        "name": "Touareg",
+        "firstPrice": 5856,
+        "secondPrice": 5130
+    },
+    {
+        "id": 3,
+        "name": "Avenger",
+        "firstPrice": 8674,
+        "secondPrice": 3118
+    },
+    {
+        "id": 4,
+        "name": "Tracer",
+        "firstPrice": 8406,
+        "secondPrice": 2382
+    },
+    {
+        "id": 5,
+        "name": "Durango",
+        "firstPrice": 2093,
+        "secondPrice": 6622
+    },
+    {
+        "id": 6,
+        "name": "968",
+        "firstPrice": 7735,
+        "secondPrice": 3681
+    },
+    {
+        "id": 7,
+        "name": "Golf",
+        "firstPrice": 9363,
+        "secondPrice": 7039
+    },
+    {
+        "id": 8,
+        "name": "Z4",
+        "firstPrice": 4827,
+        "secondPrice": 6776
+    },
+    {
+        "id": 9,
+        "name": "LaCrosse",
+        "firstPrice": 3169,
+        "secondPrice": 5229
+    },
+    {
+        "id": 10,
+        "name": "Insight",
+        "firstPrice": 9030,
+        "secondPrice": 9207
+    }
+];
+
+/**
+ * The new Array, where each element is an Object with properties: id, name and avaragePrice.
+ */
+let dob = users.map(({ id, name, firstPrice, secondPrice }) => ({ id: id, name: name, avaragePrice: (firstPrice + secondPrice) / 2 }));
+
+console.log("New Array : ");
+console.log(dob);
